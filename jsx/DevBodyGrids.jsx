0608f480fef3b44d54c57d446935f0b4e546ba3e
Yi Hu <yydxhhuyi@gmail.com>
2016-11-20 13:05:04 -0500

    joif

---
+++ jsx/DevBodyGrids.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import '../css/bootstrap.css';
+import '../css/style.css';
+import '../css/bootstrap-theme.css'
+class DevBodyGrids extends React.Component {
+   render() {
+      return (
+          <div className="DevBodyGrids">
+              <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-lg-7">
+                      <div className="panel">
+                          <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                          A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                              dlkams<br />ld malskklasic Panel
+                          A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                          
+                          </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-5">
+                      <div className="panel">
+                          <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                          A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel slk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                          
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                      <div className="col-lg-3">
+                             <div className="panel">
+                              <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                              A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                                  dlkams<br />ld malskklasic Panel
+                              A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+
+                              </div>
+                            </div>
+                        </div>
+                    <div className="col-lg-3">
+                        <div className="panel">
+                              <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                              A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel slk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+
+                              </div>
+                        </div>
+                      </div>
+                      <div className="col-lg-3">
+                        <div className="panel">
+                              <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                              A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel slk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+
+                              </div>
+                        </div>
+                      </div>
+                      <div className="col-lg-3">
+                        <div className="panel">
+                              <div className="panel-primary whitegrid">A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+                              A Blsakjdlj aslkdlj<br /> alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel alj alsjkl sjalkjd lkjasj ldjaslkj dlkajs lkjdalkjlkda<br />jslkj dlakslk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel slk dnalskndl nalksndl<br />kasnldk naslk dlkams<br />ld malskklasic Panel
+
+                              </div>
+                        </div>
+                      </div>
+                  </div>
+                  <div className="row">
+                  </div>
+              </div>
+          </div>
+      );
+   }
+}
+
+export default DevBodyGrids;
